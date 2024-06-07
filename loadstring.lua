ac1b10a745e9e53354ac578d57a8118a29247a23
EZEZEZEZZE <104246252+EZEZEZEZZE@users.noreply.github.com>
2024-06-07 05:22:25 -0500

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,7 @@
+--[[
+  shared.ReadFile = true
+  Reads "Aristois/Games/"
+  Turning it off reads off this 
+  "https://raw.githubusercontent.com/EZEZEZEZZE/Aristois/main/Games/"
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EZEZEZEZZE/Aristois/main/NewMainScript.lua", true))()
