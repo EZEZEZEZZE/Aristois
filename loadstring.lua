f458241f9805a94722f82cc513e2e34dfe805a44
EZEZEZEZZE <104246252+EZEZEZEZZE@users.noreply.github.com>
2024-06-07 05:22:53 -0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,8 @@
 --[[
-  shared.ReadFile = true
   Reads "Aristois/Games/"
   Turning it off reads off this 
   "https://raw.githubusercontent.com/EZEZEZEZZE/Aristois/main/Games/"
 ]]
+
+shared.ReadFile = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EZEZEZEZZE/Aristois/main/NewMainScript.lua", true))()
